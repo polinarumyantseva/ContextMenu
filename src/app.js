a2532831e229840d9cc7b35de7faac44f4c28e29
Polina Rumyantseva <polinrumyantseva@gmail.com>
2024-10-26 14:14:49 -0700

    add comtext menu

--- src/app.js
+++ src/app.js
@@ -1,2 +1,17 @@
-import './styles.css'
+import './styles.css';
+import { ContextMenu } from './menu';
+import { BackgroundModule } from './modules/background.module';
+
+document.addEventListener('DOMContentLoaded', () => {
+    const contextMenu = new ContextMenu('#menu');
+
+    // Пример добавления модуля
+    const bgModule = new BackgroundModule('bgModule', 'Поменять цвет');
+    contextMenu.add(bgModule.toHTML());
+
+    document.addEventListener('contextmenu', (e) => {
+        e.preventDefault();
+        contextMenu.open(e);
+    });
+})
 
--- src/menu.js
+++ src/menu.js
@@ -1,5 +1,41 @@
-import {Menu} from './core/menu'
+import { Menu } from './core/menu';
 
 export class ContextMenu extends Menu {
+    constructor(selector) {
+        super(selector);
+        this.$menuElement = document.querySelector(selector);
+    }
+    open(event) {
+        this.$menuElement.classList.add('open');
+        
+        const menuElementWidth = this.$menuElement.offsetWidth;
+        const menuElementHeight = this.$menuElement.offsetHeight;
+        
+        const windowWidth = window.innerWidth;
+        const windowHeight = window.innerHeight;
+
+        const pageX = event.pageX;
+        const pageY = event.pageY;
+        
+        if ( (windowWidth - event.screenX) < menuElementWidth ) {
+            this.$menuElement.style.left = pageX - menuElementWidth + 'px';
+        } else {
+            this.$menuElement.style.left = pageX + 'px';
+        }
+
+        if ( (windowHeight - event.screenY) < menuElementHeight ) {
+            this.$menuElement.style.top = pageY - menuElementHeight + 'px';
+        } else {
+            this.$menuElement.style.top = pageY + 'px';
+        }
+    }
+
+    close() {
+        this.$menuElement.classList.remove('open');
+    }
+
+    add(elem) {
+        this.$menuElement.innerHTML += elem;
+    }
 
 }
